7a1723a9c77f9131ae1b68a3c45e8b326ef6558e
pmpr.dev <pmpr.dev@gmail.com>
2024-10-14 16:29:17 +0000

    Obfuscated

--- src/Pmpr/Cover/Pmpr/Component/Component.php
+++ src/Pmpr/Cover/Pmpr/Component/Component.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             670d0002d08db             |
+    |             670d46a91a961             |
     |_______________________________________|
 */
  namespace Pmpr\Cover\Pmpr\Component; use Pmpr\Cover\Pmpr\Component\Module\Contact; use Pmpr\Cover\Pmpr\Container; class Component extends Container { public function mameiwsayuyquoeq() { Contact::symcgieuakksimmu(); } }
--- src/Pmpr/Cover/Pmpr/Page/ProfessionalWordpress.php
+++ src/Pmpr/Cover/Pmpr/Page/ProfessionalWordpress.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             670d0002d08db             |
+    |             670d46a91a961             |
     |_______________________________________|
 */
- namespace Pmpr\Cover\Pmpr\Page; use Pmpr\Cover\Pmpr\Setting; use Pmpr\Common\Foundation\Frontend\Page; class ProfessionalWordpress extends Page { public function qiccuiwooiquycsg() { $this->ekgmmugauoasqwyc()->wegcaymyqqoyewmw("\160\x72\157\146\145\x73\x73\x69\x6f\x6e\x61\x6c\x2d\x77\157\162\144\160\162\x65\x73\163")->gswweykyogmsyawy(__("\120\x72\157\146\145\163\163\x69\157\x6e\141\x6c\40\x57\x6f\x72\144\160\x72\x65\163\x73", PR__CVR__PMPR))->wmsaakuicamguoam($this->weysguygiseoukqw(Setting::qimsgiaecyaykeyu)); parent::qiccuiwooiquycsg(); } }
+ namespace Pmpr\Cover\Pmpr\Page; use Pmpr\Cover\Pmpr\Setting; use Pmpr\Common\Foundation\Frontend\Page; class ProfessionalWordpress extends Page { public function qiccuiwooiquycsg() { $this->ekgmmugauoasqwyc()->wegcaymyqqoyewmw("\160\162\x6f\x66\145\x73\x73\151\157\x6e\x61\154\55\167\x6f\x72\144\x70\162\x65\x73\163")->gswweykyogmsyawy(__("\x50\x72\x6f\x66\x65\163\163\151\157\156\x61\x6c\40\x57\x6f\x72\144\160\x72\x65\163\163", PR__CVR__PMPR))->wmsaakuicamguoam($this->weysguygiseoukqw(Setting::qimsgiaecyaykeyu)); parent::qiccuiwooiquycsg(); } }
