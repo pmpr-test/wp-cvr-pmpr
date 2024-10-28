bad45c093494097cc4d8874a808341bd1b308889
pmpr.dev <pmpr.dev@gmail.com>
2024-10-28 16:49:36 +0000

    Obfuscated

--- src/Pmpr/Cover/Pmpr/Container.php
+++ src/Pmpr/Cover/Pmpr/Container.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             6714e16e2f881             |
+    |             671fc0678d0ad             |
     |_______________________________________|
 */
  namespace Pmpr\Cover\Pmpr; use Pmpr\Common\Foundation\Container\Container as BaseClass; use Pmpr\Cover\Pmpr\Traits\CommonTrait; abstract class Container extends BaseClass { use CommonTrait; }
--- src/Pmpr/Cover/Pmpr/Page/Reportage.php
+++ src/Pmpr/Cover/Pmpr/Page/Reportage.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             6714e16e2f881             |
+    |             671fc0678d0ad             |
     |_______________________________________|
 */
- namespace Pmpr\Cover\Pmpr\Page; use Pmpr\Cover\Pmpr\Setting; use Pmpr\Common\Foundation\Frontend\Page; class Reportage extends Page { public function qiccuiwooiquycsg() { $this->wegcaymyqqoyewmw("\x72\145\x70\x6f\162\164\141\147\145")->gswweykyogmsyawy(__("\x52\x65\x70\x6f\x72\164\141\147\x65", PR__CVR__PMPR))->wmsaakuicamguoam($this->weysguygiseoukqw(Setting::akmweacgkkukkakq)); } }
+ namespace Pmpr\Cover\Pmpr\Page; use Pmpr\Cover\Pmpr\Setting; use Pmpr\Common\Foundation\Frontend\Page; class Reportage extends Page { public function qiccuiwooiquycsg() { $this->wegcaymyqqoyewmw("\162\x65\160\157\162\x74\x61\147\145")->gswweykyogmsyawy(__("\x52\145\x70\x6f\162\164\x61\147\x65", PR__CVR__PMPR))->wmsaakuicamguoam($this->weysguygiseoukqw(Setting::akmweacgkkukkakq)); } }
